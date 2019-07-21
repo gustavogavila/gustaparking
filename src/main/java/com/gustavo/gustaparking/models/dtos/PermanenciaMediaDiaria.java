bad9868d56f928176b79a90235294bd85f8b308d
Gustavo <gustavogavila01@gmail.com>
2019-07-21 01:41:36 -0300

    Faltou comitar o dto.

---
+++ src/main/java/com/gustavo/gustaparking/models/dtos/PermanenciaMediaDiaria.java
@@ -0,0 +1,19 @@
+package com.gustavo.gustaparking.models.dtos;
+
+public class PermanenciaMediaDiaria {
+
+	private Double permanenciaMediaDiaria;
+
+	public PermanenciaMediaDiaria(Double permanenciaMediaDiaria) {
+		this.permanenciaMediaDiaria = permanenciaMediaDiaria;
+	}
+
+	public Double getPermanenciaMediaDiaria() {
+		return permanenciaMediaDiaria;
+	}
+
+	public void setPermanenciaMediaDiaria(Double permanenciaMediaDiaria) {
+		this.permanenciaMediaDiaria = permanenciaMediaDiaria;
+	}
+
+}
